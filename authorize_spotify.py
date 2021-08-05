132d7d5069af81abc77f8731d0994e58903a76fa
wsztejnberg <Will.Sztejnberg@delawarelife.com>
2021-08-05 01:04:38 -0500

    Continue authorization development

--- authorize_spotify.py
+++ authorize_spotify.py
@@ -1,7 +1,7 @@
-from time import time
+import time
 import spotipy
 from flask import Flask, redirect, session, request
-from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
 import os
 import dotenv
 from dotenv import load_dotenv
@@ -10,46 +10,55 @@ from dotenv import load_dotenv
 app = Flask(__name__)
 auth_code = ''
 
+app.secret_key = 'SOMETHING-RANDOM'
+app.config['SESSION_COOKIE_NAME'] = 'spotify-login-session'
+
 
 @app.route('/')
 def request_auth():
-    request = create_spotify_oauth()
-    auth_url = request.get_authorize_url()
+    sp_oauth = create_spotify_oauth()
+    auth_url = sp_oauth.get_authorize_url()
     print('auth_url: ' + str(auth_url))
     return redirect(auth_url)
 
 
 @app.route('/spotify/callback')
 def spotify_callback():
+    sp_oauth = create_spotify_oauth()
     session.clear()
-    code = request.args.to_dict().get('code')
-    token_info = request.get_access_token(code)
+    code = request.args.get('code')
+    token_info = sp_oauth.get_access_token(code)
     session['token_info'] = token_info
-    return redirect("/test")
+    return redirect('/test')
+
 
 @app.route('/test')
 def test_write():
     session['token_info'], authorized = verify_token()
     session.modified = True
+    print("authorized: " + str(authorized))
     if not authorized:
         return redirect('/')
 
     sp = spotipy.Spotify(auth=session.get('token_info').get('access_token'))
+    user_id = sp.me()['id']
+    sp.user_playlist_create(user_id, "spotify_playlist", True, False, "this playlist was made with code!")
+    return "Playlist has been created!"
 
 
 def verify_token():
     token_valid = False
     token_info = session.get('token_info', {})
 
-    if not (session.get('token_info') is None):
+    if not (session.get('token_info', False)):
         return token_info, token_valid
 
     now = int(time.time())
     is_token_expired = session.get('token_info').get('expires_at') - now < 60
 
-    if (is_token_expired):
-        response = create_spotify_oauth()
-        token_info = response.refresh_access_token(session.get('token_info').get('refresh_token'))
+    if is_token_expired:
+        sp_oauth = create_spotify_oauth()
+        token_info = sp_oauth.refresh_access_token(session.get('token_info').get('refresh_token'))
 
     token_valid = True
     return token_info, token_valid
